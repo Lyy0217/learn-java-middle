5e39926207d68b00402ed25583be790621d42097
Lyy <liuyangyang09@meituan.com>
2020-03-08 01:18:54 +0800

    log4j

--- src/main/java/com/lyy/learn/java/application/log4j/TestLog4j.java
+++ src/main/java/com/lyy/learn/java/application/log4j/TestLog4j.java
@@ -4,20 +4,29 @@ package com.lyy.learn.java.application.log4j;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.xml.DOMConfigurator;
 
 public class TestLog4j {
-
+    //基于类的名称获取日志对象
     static Logger logger = Logger.getLogger(TestLog4j.class);
 
+
     public static void main(String[] args) throws InterruptedException {
-        BasicConfigurator.configure();
-        logger.setLevel(Level.DEBUG);
-        logger.trace("跟踪信息");
-        logger.debug("调试信息");
-        logger.info("输出信息");
-        Thread.sleep(1000);
-        logger.warn("警告信息");
-        logger.error("错误信息");
-        logger.fatal("致命信息");
+        //进行默认配置
+//        BasicConfigurator.configure();
+
+        //Log4j的配置方式按照log4j.properties中的设置进行
+//        PropertyConfigurator.configure("/Users/lyy0217/Documents/learn/how2j/learn-java-middle/src/main/profiles/log4j.properties");
+        DOMConfigurator.configure("/Users/lyy0217/Documents/learn/how2j/learn-java-middle/src/main/profiles/log4j.xml");
+
+        for (int i = 0; i < 5000; i++) {
+            logger.trace("跟踪信息");
+            logger.debug("调试信息");
+            logger.info("输出信息");
+            logger.warn("警告信息");
+            logger.error("错误信息");
+            logger.fatal("致命信息");
+        }
     }
 }
